5722fa416fd21c4982fd7700646c5c5a70bcb069
Dalil Ali <dalil.ali@gmx.de>
2023-11-10 13:37:07 +0100

    Code Refactoring

--- getCountryData.js
+++ getCountryData.js
@@ -18,68 +18,14 @@ async function fetchDataFromAPI() {
             let template = '';
             for (x in data) {
                 count++;
-
-                if (data[x].region === "Africa") {
-                    template += '<div class="col africa"><h3>' 
-                    + data[x].flag +" "
-                    + data[x].name.common + '</h3><ul class="detail"><li>' 
-                    + data[x].region + '</li><li>' 
-                    + data[x].capital + '</li><li>' 
+                template += '<div class="col ' + data[x].region + '"><h3>'
+                    + data[x].flag + " "
+                    + data[x].name.common + '</h3><ul class="detail"><li>'
+                    + data[x].region + '</li><li>'
+                    + data[x].capital + '</li><li>'
                     + data[x].population + '</li><li>'
-                    + data[x].flag + '</li></ul>' 
+                    + data[x].flag + '</li></ul>'
                     + '</div>';
-                    continue;
-                }
-                else if (data[x].region === "Asia") {
-                    template += '<div class="col asia"><h3>' + data[x].flag +" "
-                    + data[x].name.common + '</h3><ul class="detail"><li>' 
-                    + data[x].region + '</li><li>' 
-                    + data[x].capital + '</li><li>' 
-                    + data[x].population + '</li><li>'
-                    + data[x].flag + '</li></ul>' 
-                    + '</div>';
-                    continue;
-                }
-                else if (data[x].region === "Americas") {
-                    template += '<div class="col amerika"><h3>' + data[x].flag +" "
-                    + data[x].name.common + '</h3><ul class="detail"><li>' 
-                    + data[x].region + '</li><li>' 
-                    + data[x].capital + '</li><li>' 
-                    + data[x].population + '</li><li>'
-                    + data[x].flag + '</li></ul>' 
-                    + '</div></div>';
-                    continue;
-                }
-                else if (data[x].region === "Oceania") {
-                    template += '<div class="col oceania"><div class="p-3 "><h3>' + data[x].flag +" "
-                    + data[x].name.common + '</h3><ul class="detail"><li>' 
-                    + data[x].region + '</li><li>' 
-                    + data[x].capital + '</li><li>' 
-                    + data[x].population + '</li><li>'
-                    + data[x].flag + '</li></ul>' 
-                    
-                    + '</div></div>';
-                    continue;
-                }
-                else if (data[x].region === "Europe") {
-                    template += '<div class="col europe"><h3>' + data[x].flag +" "
-                    + data[x].name.common + '</h3><ul class="detail"><li>' 
-                    + data[x].region + '</li><li>' 
-                    + data[x].capital + '</li><li>' 
-                    + data[x].flag + '</li><li>' 
-                    + data[x].population + '</li><ul>'
-                    + '</div>';
-                    continue;
-                }
-                else {
-                    template += '<div class="col "><h3>' + data[x].flag +" "
-                    + data[x].name.common + '</h3><ul class="detail"><li>' 
-                    + data[x].region + '</li><li>' 
-                    + data[x].capital + '</li><li>' 
-                    + data[x].flag + '</li><li>' 
-                    + data[x].population + '</li></ul>'
-                    + '</div>';
-                }
             }
             document.getElementById("allcountry").innerHTML = template;
             console.log('Data fetched successfully:' + count);
@@ -91,3 +37,7 @@ async function fetchDataFromAPI() {
         console.error(error);
     }
 }
+
+window.addEventListener("load", event => {
+    return;
+})
