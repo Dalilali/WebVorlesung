eeddc42ab6a49e45d5450a61650bc386f903b50e
Dalil Ali <dalil.ali@gmx.de>
2023-11-11 22:26:40 +0100

    some fixes

--- app.js
+++
@@ -1,6 +0,0 @@
-async function chco() {
-  const res = await fetch('https://dummyjson.com/products/categories');
-  const data = await res.json();
-  document.getElementById('panelsStayOpen-collapseOne').innerHTML = JSON.stringify(data);
-  return result_1;
-}
